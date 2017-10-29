335adc50ef355e30234d789b564bdb07e0987f63
Michalis Zamparas <mikezaby@gmail.com>
2017-10-29 14:55:13 +0200

    Initial file structure and config for js and css

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,24 +1,53 @@
 var path = require('path');
 
+var production = process.env.NODE_ENV === 'production';
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-  entry: './application.js',
+  entry: {
+    application: ['./app/javascripts/application'],
+  },
   output: {
     path: path.resolve(__dirname, '..', 'public'),
-    filename: 'application.js'
+    filename: production ? '[name]-[chunkhash].js' : '[name].js',
+    chunkFilename: production ? '[name]-[chunkhash].js' : '[name].js',
+  },
+  resolve: {
+    modules: [
+      path.join(__dirname, '..', 'app'),
+      'node_modules',
+    ],
+    extensions: ['.js', '.scss', '.css'],
   },
 
   module: {
-    rules: [{
-      test: /\.scss$/,
-      use: [{
-        loader: "style-loader" // creates style nodes from JS strings
-      }, {
-        loader: "css-loader" // translates CSS into CommonJS
-      }, {
-        loader: "sass-loader" // compiles Sass to CSS
-      }]
-    }]
-  }
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader',
+            'sass-loader'
+          ]
+        })
+      }
+    ]
+  },
+
+  plugins: [
+    new ExtractTextPlugin({
+      filename: production ? '[name]-[chunkhash].css' : '[name].css'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html'
+    })
+  ],
+
+  stats: {
+    colors: true
+  },
 
   devtool: 'source-map'
 };
