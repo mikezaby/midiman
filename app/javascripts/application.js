532d529831a0f182d1711b433f2687f5dd3a5323
Michalis Zamparas <mikezaby@skroutz.gr>
2017-11-04 15:35:36 +0200

    Add proof of concept midi usage

--- app/javascripts/application.js
+++ app/javascripts/application.js
@@ -4,3 +4,18 @@ import 'styles/application.scss';
 import MidiDevice from 'midi_device';
 
 window.MidiDevice = MidiDevice;
+
+const triggerKeyboard = (type, note, octave) => {
+  console.log(type, note, octave)
+  const octaveEl = document.querySelector(`.octave:nth-child(${octave-2}`)
+  const noteEl = octaveEl.querySelector(`.${note}`)
+
+  noteEl.classList.toggle('active', type === 'noteOn')
+}
+
+const init = async () => {
+  const midi = await MidiDevice.find('FEC3FB776CFAC58F6E504F4F035731965BEBDEB2CC68E6EAFDE649B32E3B55F7');
+  midi.onNote(triggerKeyboard);
+};
+
+init();
--- app/javascripts/midi_device.js
+++ app/javascripts/midi_device.js
@@ -1,3 +1,10 @@
+const NOTES = ['c', 'c-sharp', 'd', 'd-sharp', 'e', 'f', 'f-sharp', 'g', 'g-sharp', 'a', 'a-sharp', 'b'];
+
+const TYPES = {
+  8: 'noteOff',
+  9: 'noteOn'
+};
+
 export default class MidiDevise {
   static async devices() {
     return (await this.inputs()).map((input) => new MidiDevise(input));
@@ -21,6 +28,44 @@ export default class MidiDevise {
   constructor(midi) {
     this.id = midi.id;
     this.name = midi.name;
-    this.midi = midi;
+    this._midi = midi;
+    this.connect();
+  }
+
+  connect() {
+    this._midi.onmidimessage = (e) => this.processEvent(e);
+  }
+
+  processEvent(e) {
+    const data = e.data;
+    const type = this.type(data);
+
+    switch (type) {
+    case 'noteOn':
+    case 'noteOff':
+      this.noteCallback(type, this.note(data[1]), this.octave(data[1]));
+    }
+  }
+
+  onNote(callback) {
+    this.noteCallback = callback;
+  }
+
+  type(data) {
+    const type = TYPES[data[0] >> 4];
+
+    if (type === 'noteOn' && data[2] === 0) {
+      return 'noteOff';
+    }
+
+    return type;
+  }
+
+  note(num) {
+    return NOTES[num % 12];
+  }
+
+  octave(num) {
+    return Math.floor(num / 12);
   }
 }
